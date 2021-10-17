5fdd660efaa7bcfef9d371147a6fea5e5a2b238a
Rach Gee <Rach-Gee@hotmail.com>
2021-10-17 10:34:45 +0800

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    'index.js',
+    "/index.js",
     "/manifest.json",
     "/styles.css",
     "/icons/icon-192x192.png",
