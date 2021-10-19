4211f3c391973b69ac19c10aa64bee86693a2e1c
Rach Gee <Rach-Gee@hotmail.com>
2021-10-19 18:31:59 +0800

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,14 +16,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useFindAndModify: false,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+  });
 
 // routes
 app.use(require("./routes/api.js"));
